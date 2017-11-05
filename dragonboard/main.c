4135d5e4e082e9891adfecb3984372651283db2c
sjfricke <sjfricke@wisc.edu>
2017-11-05 16:59:42 +0000

    whatever

--- dragonboard/main.c
+++ dragonboard/main.c
@@ -1,10 +1,10 @@
-//#include "voice.h"
+#include "audio/voice.h"
 #include "hardware/gpio.h"
 #include "hardware/accel.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <assert.h>
-
+#include <pthread.h>
 #define LED_COUNT 8
 
 static int status;
@@ -15,7 +15,8 @@ static int busy;
 // assumes length is greater than 1
 void LightRing(char** leds, int length, int delay) {
   int i;
-  
+
+ 
   GpioSetValue(leds[0], 1);
   for (i = 1; i < length; i++) {
     //    printf("LightRing: %s (%d)\n", leds[i], i);
@@ -26,13 +27,13 @@ void LightRing(char** leds, int length, int delay) {
   usleep(delay);
   // printf("LightRing END: %s\n", leds[length-1]);
   GpioSetValue(leds[length-1], 0);
+ 
+  
 }
 
 int main ( int argc, char* argv[] ) {
-
   int i;
-  float accel_x, accel_y, accel_z;
-  
+    char* voice = malloc(1024);
   char** led_line;
   led_line =(char*)malloc(sizeof(char*) * LED_COUNT);
   for (i = 0; i < LED_COUNT; i++) {
@@ -43,8 +44,8 @@ int main ( int argc, char* argv[] ) {
   
   led_line[0] = GpioDB410cMapping(25);
   led_line[1] = GpioDB410cMapping(26);
-  led_line[2] = GpioDB410cMapping(29);
-  led_line[3] = GpioDB410cMapping(23);
+  led_line[2] = GpioDB410cMapping(23);
+  led_line[3] = GpioDB410cMapping(24);
   led_line[4] = GpioDB410cMapping(31);
   led_line[5] = GpioDB410cMapping(32);
   led_line[6] = GpioDB410cMapping(33);
@@ -56,20 +57,21 @@ int main ( int argc, char* argv[] ) {
     GpioSetDirection(led_line[i], 1);
   }
   printf("Set direction GPIO\n");
-  
-  // get PocketSphinx working
-  //  setupVoiceDictionary();
 
+  
   GpioSetAllValue(led_line, LED_COUNT, 0); //clears
 
-  AccelSetup();
+
+  // get PocketSphinx working
+  setupVoiceDictionary();
+
+  //  AccelSetup();
   
   while (1) {
-    //LightRing(led_line, LED_COUNT, 100000);
-    AccelGetValue(&accel_x, &accel_y, &accel_z); 
-    printf("Acceleration: x = %f y = %f z = %f\n\n", accel_x, accel_y, accel_z );
-
-    usleep(500 * 1000);
+    //    i = pthread_create(&threadT, NULL, LightRing, (void*) NULL);
+    LightRing(led_line, LED_COUNT, 1000000);
+    voice = voiceCommand();
+    printf("%s\n", voice);
   } 
  
 }
